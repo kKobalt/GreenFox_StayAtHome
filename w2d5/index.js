bfee141402301de6d187423cb9c5aff9fd003ab2
kKobalt <32681344+kKobalt@users.noreply.github.com>
2020-04-27 00:14:38 +0200

    JS

--- w2d5/index.js
+++ w2d5/index.js
@@ -1,4 +1,15 @@
-let name = "Andy";
-let surname = "Williams";
+let age = 23;
+let ww2 = 6;
+let teslaSharePrice = 725.15;
+let teslaShareAmount = 76;
+let tesla = teslaShareAmount * teslaSharePrice;
+let secondsOfDay = 24 * 60 * 60;
+let hungarians = (9660351 / 7780473000) * 100;
+let chinaSize = 9597000;
+let czechSize = 78865;
 
-console.log(name + " " + surname);
+let bigger = chinaSize / czechSize;
+
+console.log(teslaShareAmount + " shares of Tesla equals $" + tesla + " at April 27th, 2020");
+console.log(hungarians + " % world population are Hungarians");
+console.log("China is " + bigger + " times bigger than Czech Republic");
